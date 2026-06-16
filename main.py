b4de703945146f934e42f189de3c5f26e8a867e6
Hi-Im-Yuri <tavernajustin@gmail.com>
2026-06-16 12:07:48 -0400

    added shoot functionality
    added the shot object and allow player to shoot with space bar

--- main.py
+++ main.py
@@ -1,20 +1,27 @@
 import pygame, sys
+
 from logger import log_state, log_event
 from constants import SCREEN_HEIGHT, SCREEN_WIDTH
+
 from player import Player
 from asteroid import Asteroid
 from asteroidfield import AsteroidField
+from shot import Shot
 
 def main():
     pygame.init()
     clock = pygame.time.Clock()
     dt = 0.0
+
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
     asteroids = pygame.sprite.Group()
+    shots = pygame.sprite.Group()
+
     Player.containers = (updatable, drawable)
     Asteroid.containers = (asteroids, updatable, drawable)
     AsteroidField.containers = (updatable)
+    Shot.containers = (shots, updatable, drawable)
 
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
